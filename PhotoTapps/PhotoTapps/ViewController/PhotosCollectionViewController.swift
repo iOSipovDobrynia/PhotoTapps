d00a3e50668c5c5afd927a9d6c385e361f0048be
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-01 20:54:13 +0300

    add UICollectionViewDelegateFlowLayout

---
+++ PhotoTapps/PhotoTapps/ViewController/PhotosCollectionViewController.swift
@@ -0,0 +1,58 @@
+//
+//  PhotosCollectionViewController.swift
+//  PhotoTapps
+//
+//  Created by Goodwasp on 01.10.2023.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+final class PhotosCollectionViewController: UICollectionViewController {
+
+    // MARK: - View's life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+extension PhotosCollectionViewController {
+    
+}
+
+// MARK: - UICollectionViewDataSource
+extension PhotosCollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemsPerRow: CGFloat = 3
+        let paddingWidth = 20 * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        20
+    }
+}
---
+++ PhotoTapps/PhotoTapps/Views/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Goodwasp on 01.10.2023.
+//
+
+import UIKit
+
+final class PhotoCell: UICollectionViewCell {
+    
+}
